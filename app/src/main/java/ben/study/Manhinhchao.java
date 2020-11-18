b465bb67e0d71a8efd401033aae42dd1ac4ef0cd
vininh0600 <59503742+vininh0600@users.noreply.github.com>
2020-11-18 18:39:53 +0700

    ninh4

--- app/src/main/java/ben/study/Manhinhchao.java
+++ app/src/main/java/ben/study/Manhinhchao.java
@@ -1,8 +1,11 @@
 package ben.study;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import ben.study.codeduan1.R;
 
@@ -11,6 +14,18 @@ public class Manhinhchao extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_manhinhchao);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_manhinhchao);
+        //chuyển màn
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new
+                        Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 }
