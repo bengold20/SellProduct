dde9a9e3a4db3ef26e69936661597a2e5bbca823
damvanben <58337529+damvanben@users.noreply.github.com>
2020-11-25 20:26:17 +0700

    thử xem

--- app/src/main/java/ben/study/theLoai/TheLoaiScreen.java
+++ app/src/main/java/ben/study/theLoai/TheLoaiScreen.java
@@ -1,12 +1,16 @@
 package ben.study.theLoai;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import ben.study.codeduan1.Home;
 import ben.study.codeduan1.R;
 
 public class TheLoaiScreen extends AppCompatActivity {
@@ -37,10 +41,32 @@ public class TheLoaiScreen extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.item_theloai,menu);
+        return true;
+    }
 
-    public void themHoaDon(Menu item){
-        Intent intent = new Intent(TheLoaiScreen.this,ThemSua_TheLoai.class);
-        startActivity(intent);
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_search:
+                Toast.makeText(this,"tìm kiếm đi",Toast.LENGTH_LONG).show();
+            break;
+            case R.id.item_ThemTheLoai:
+                Intent intent = new Intent(TheLoaiScreen.this,ThemSua_TheLoai.class);
+                startActivity(intent);
+                break;
+            case R.id.item_SuaTheLoai:
+                Intent intent1 = new Intent(TheLoaiScreen.this,ThemSua_TheLoai.class);
+                startActivity(intent1);
+                break;
+            case R.id.item_TrangChuTheLoai:
+                Intent intent2 = new Intent(TheLoaiScreen.this, Home.class);
+                startActivity(intent2);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
